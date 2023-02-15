c72381eb0a6204e2c63e0b5ac72ed49bc32810d6
wnsghks <kjhwan223@naver.com>
2023-02-16 03:46:19 +0900

    update_ex)2

--- ex1)/click.js
+++
@@ -1,9 +0,0 @@
-const menuToggle = document.querySelector(".menuToggle");
-const navigation = document.querySelector(".navigation");
-
-function changeForm() {
-    menuToggle.classList.toggle("action");
-    navigation.classList.toggle("action");
-}
-
-menuToggle.addEventListener("click", changeForm);
